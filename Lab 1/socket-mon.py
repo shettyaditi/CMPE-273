b6d1f8a1d56f531c4cff08aad988369020b805a5
Aditi Shetty <aditi.shetty@sjsu.edu>
2017-02-17 00:01:11 -0800

    Uploaded Code

--- Lab 1/socket-mon.py
+++ Lab 1/socket-mon.py
@@ -1,1 +1,28 @@
 
+import psutil
+import heapq
+import csv
+from operator import itemgetter, attrgetter
+from collections import namedtuple
+from collections import Counter
+
+class pid_sorted :
+    def __init__(self, x, ):
+        self.user_id = y
+
+    def __repr__(self):
+       return str(self.pid)
+
+n = psutil.net_connections()
+
+SocketConn = namedtuple('sconn', ['fd', 'family', 'type', 'laddr', 'raddr', 'status', 'pid'])
+output = []
+for i in n:
+    x = SocketConn(*i)
+    output.append(x.pid)
+
+print("PID and its frequency")
+counter = Counter(output)
+top_hundred = counter.most_common(100)
+for i in top_hundred:
+    print(i)
