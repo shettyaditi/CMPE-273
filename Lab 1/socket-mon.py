ce965b97333106bdebe1dc4d70f371b05665f9eb
Aditi Shetty <aditi.shetty@sjsu.edu>
2017-02-17 23:08:27 -0800

    Updated code

--- Lab 1/socket-mon.py
+++ Lab 1/socket-mon.py
@@ -1,28 +1,30 @@
-
 import psutil
-import heapq
-import csv
 from operator import itemgetter, attrgetter
 from collections import namedtuple
-from collections import Counter
+from collections import OrderedDict
+from itertools import groupby
 
-class pid_sorted :
-    def __init__(self, x, ):
-        self.user_id = y
-
-    def __repr__(self):
-       return str(self.pid)
-
-n = psutil.net_connections()
+n = psutil.net_connections(kind="tcp")
 
 SocketConn = namedtuple('sconn', ['fd', 'family', 'type', 'laddr', 'raddr', 'status', 'pid'])
 output = []
+final = {}
 for i in n:
     x = SocketConn(*i)
-    output.append(x.pid)
+    if x.laddr and x.raddr:
+        output.append((str(x.pid), "\"" + str(x.pid) + "\",\"" + str(x.laddr[0]) + "@" + str(x.laddr[1]) + "\",\"" + str(x.raddr[0]) + "@" + str(x.raddr[1]) + "\",\"" + x.status + "\""))
 
-print("PID and its frequency")
-counter = Counter(output)
-top_hundred = counter.most_common(100)
-for i in top_hundred:
-    print(i)
+for key, group in groupby(sorted(output), lambda x: x[0]):
+    for i in group:
+        if key in final:
+            final[key].append(i[1])
+        else:
+            final[key] = [i[1]]
+
+finalDict = OrderedDict(sorted(final.items(), key=lambda x: (len(x[1]), x[0]), reverse=True))
+
+print "\"pid\",\"laddr\",\"raddr\",\"status\""
+
+for key, value in finalDict.items():
+    for process in value:
+        print (process)
